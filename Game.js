13a605b8a161578f6408d3e75f635cbedf47dba1
DEMENYACA <39350941+DEMENYACA@users.noreply.github.com>
2019-02-06 23:07:21 -0500

    Add files via upload
    added button

--- Game.js
+++ Game.js
@@ -8,88 +8,88 @@ console.log ("Motivational Words");
 document.getElementById("love").innerhtml=getHatred */}
 
 
-// {/* <script>
+{/* <script>
 
-// </script> */}
+</script> */}
    
 
 
-// {/* // answer=prompt ('Love for all ___ for none...Khalifatul Masih III');
-// // if (answer=="hatred") { */}
-// //     alert("Great! Stay Motivated!");
-// // }else if (answer=="Hate") { 
-//     alert("Not quite, try again!"); 
-// }else {
-//     alert("Motivate Up!");
-// }
-// console.log ("Motivational Words");
+{/* // answer=prompt ('Love for all ___ for none...Khalifatul Masih III');
+if (answer=="hatred") { */}
+    alert("Great! Stay Motivated!");
+}else if (answer=="Hate") { 
+    alert("Not quite, try again!"); 
+}else {
+    alert("Motivate Up!");
+}
+console.log ("Motivational Words");
 
-// answer=prompt ('___ kills more dreams than failure ever will...Suze Kassem');
-// if (answer=="Doubt"){
-//  alert("Great! Stay Motivated!");
-// } else if(answer=="Fear"){
-//    alert ("Not quite, try again!");
-// } else {
-//     alert("Motivate Up!");
-// }
-// console.log ("Motivational Words");
-// answer=prompt('Die with ___not dreams...Unknown');
-// if(answer=="memories"){
-//     alert("Great! Stay Motivated!");
-// }else if (answer=="love"){
-//     alert("Not quite,try again! ");
-// }else {
-//     alert("Motivate Up!");
-// }
-// console.log ("Motivational Words");
-// answer=prompt("Everything you can ___is real! Pablo Picasso");
+answer=prompt ('___ kills more dreams than failure ever will...Suze Kassem');
+if (answer=="Doubt"){
+ alert("Great! Stay Motivated!");
+} else if(answer=="Fear"){
+   alert ("Not quite, try again!");
+} else {
+    alert("Motivate Up!");
+}
+console.log ("Motivational Words");
+answer=prompt('Die with ___not dreams...Unknown');
+if(answer=="memories"){
+    alert("Great! Stay Motivated!");
+}else if (answer=="love"){
+    alert("Not quite,try again! ");
+}else {
+    alert("Motivate Up!");
+}
+console.log ("Motivational Words");
+answer=prompt("Everything you can ___is real! Pablo Picasso");
 
 
-// if(answer=="imagine"){
-//     alert ('Great! Stay Motivated!');
-// }else if(answer=="see"){
-//     alert("Not quite, try again!");
-// }else {
-//          alert("Motivate Up!")
-// }
-// console.log ("Motivational Words");
-// answer=prompt('___is the ulimate sophistication...Leanardo da Vinci');
-// if(answer=="Simplicity"){
-//     alert("Great! Stay Motivated!");
-// }else if (answer=="art"){
-//     alert("Not quite, try again!");
-// }else{
-//     alert("Motivate Up");
-// }
+if(answer=="imagine"){
+    alert ('Great! Stay Motivated!');
+}else if(answer=="see"){
+    alert("Not quite, try again!");
+}else {
+         alert("Motivate Up!")
+}
+console.log ("Motivational Words");
+answer=prompt('___is the ulimate sophistication...Leanardo da Vinci');
+if(answer=="Simplicity"){
+    alert("Great! Stay Motivated!");
+}else if (answer=="art"){
+    alert("Not quite, try again!");
+}else{
+    alert("Motivate Up");
+}
 
-// answer=prompt('What we ____we become...Buddha');
-// if (answer=="think"){
-//    alert ("Great! Stay Motivated!");  
-// }else if (answer=="love"){
-//    alert ("Not quite, try again!");
-// }else{
-//    alert ("Motivate Up!");
-// }
+answer=prompt('What we ____we become...Buddha');
+if (answer=="think"){
+   alert ("Great! Stay Motivated!");  
+}else if (answer=="love"){
+   alert ("Not quite, try again!");
+}else{
+   alert ("Motivate Up!");
+}
  
-// answer=prompt('Yesterday you said ...___Just do it!...Nike.');
-// if(answer=="tommorow"){
-//     alert("Great! Stay Motivated!");
-// }else if (answer=="today"){
-//     alert("No quite, try again!");
-// }else
-//     alert("Motivate Up!");
+answer=prompt('Yesterday you said ...___Just do it!...Nike.');
+if(answer=="tommorow"){
+    alert("Great! Stay Motivated!");
+}else if (answer=="today"){
+    alert("No quite, try again!");
+}else
+    alert("Motivate Up!");
 
-// // answer=prompt("Would you like to play again?____");
-// // if(answer=="yes"){
-// //     alert("Click ok!");
-// // }else if (answer=="no");
-// //     alert("Thanks for playing!");
+answer=prompt("Would you like to play again?____");
+if(answer=="yes"){
+    alert("Click ok!");
+}else if (answer=="no");
+    alert("Thanks for playing!");
 
 
-// let userName= "Motivational Words!";
-//     function showMessage() {
-//   let message= 'Thanks for playing ' + userName;
-//      alert (message);
+let userName= "Motivational Words!";
+    function showMessage() {
+  let message= 'Thanks for playing ' + userName;
+     alert (message);
 }
 showMessage();
 
